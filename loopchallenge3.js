414d636f5741c74f7951901dae6140f883d33c33
bbabilya <bjbabilya@gmail.com>
2022-05-20 16:07:53 -0500

    loopchallenges commit

---
+++ loopchallenge3.js
@@ -0,0 +1,5 @@
+var array = [4, 2.5, 1, -0.5, -2, -3.5]
+
+for(var i = 0; i < array.length; i++){
+    console.log(array[i]);
+}
---
+++ loopchallenge4.js
@@ -0,0 +1,8 @@
+var sum = 0
+
+for(var i = 1; i <=100; i++){
+    sum += i;
+
+}
+
+console.log(sum);
